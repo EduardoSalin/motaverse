957846cadfb872b79faf3fd4c5b4c8415ccd963f
Eduardo <esalin11@msudenver.edu>
2023-12-01 12:52:45 -0700

    pep 8 compliance

--- temp.py
+++ temp.py
@@ -2,12 +2,13 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
-
 class SignUpForm(FlaskForm):
     id = StringField('Username', validators=[DataRequired()])
     name = StringField('Name', validators=[DataRequired()])
-    profile_picture = SelectField('Profile Picture', choices=[
-            ('pic1.jpg', 'Picture 1'),
+    profile_picture = SelectField(
+        'Profile Picture', 
+        choices=[
+            ('pic1.jpg', 'Picture 1'), 
             ('pic2.jpg', 'Picture 2'),
             ('pic3.jpg', 'Picture 3'),
             ('pic4.jpg', 'Picture 4'),
@@ -15,10 +16,9 @@ class SignUpForm(FlaskForm):
             ('pic6.jpg', 'Picture 6'),
             ('pic7.jpg', 'Picture 7'),
             ('pic8.jpg', 'Picture 8')])
-
+    
     passwd = PasswordField('Password', validators=[DataRequired()])
-    passwd_confirm = PasswordField('Confirm Password',
-                                   validators=[DataRequired()])
+    passwd_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Confirm')
 
 
@@ -36,3 +36,10 @@ class PostForm(FlaskForm):
 class CommentForm(FlaskForm):
     content = StringField('Content', validators=[DataRequired()])
     submit = SubmitField('Comment')
+    '''
+CS3250 - Software Development Methods and Tools - Fall 2023
+Instructor: Thyago Mota
+Students: 
+Brady Lamson, Emerson Hatton, Riley Moen, Ebenezer Addei, Eduardo Salinas
+Description: motaverse - Forms for the SQLAlchemy application
+'''
