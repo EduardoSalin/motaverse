3ff6885b39f93a0e95c11ef64a232da2dc9acf5e
emerson hatton <ehatton@msudenver.edu>
2023-12-01 11:29:17 -0700

    added database likes stuff, basic display element.

--- app/models.py
+++ app/models.py
@@ -16,18 +16,24 @@ class User(db.Model, UserMixin):
         "polymorphic_identity": "user"
     }
 
+    liked_posts = db.relationship('Post', secondary='post_likes', back_populates='likes')
 
-
-class Post(db.Model, UserMixin):
+class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     content = db.Column(db.String)
-    likes = db.Column(db.Integer)
+    
+    # Change likes to be a relationship with the User model
+    likes = db.relationship('User', secondary='post_likes', back_populates='liked_posts')
 
-    user = db.Relationship('User', back_populates='posts')
-    # comments = db.relationship('Comment', back_populates='')
+    user = db.relationship('User', back_populates='posts')
 
+# Create a new table to represent the many-to-many relationship between users and liked posts
+post_likes = db.Table('post_likes',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('post_id', db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+)
 '''
 class Comment(db.Model, UserMixin):
     __tablename__ = 'comments'
