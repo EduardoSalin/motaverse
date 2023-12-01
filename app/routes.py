e792b7411e9222d6901e04afa299c040e9aa425e
Eduardo <esalin11@msudenver.edu>
2023-12-01 13:11:58 -0700

    pep 8 compliace

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 '''
 CS3250 - Software Development Methods and Tools - Fall 2023
 Instructor: Thyago Mota
-Students: Brady Lamson, Emerson Hatton, Riley Moen, Ebenezer Addei, Eduardo Salinas
+Students:
+Brady Lamson, Emerson Hatton, Riley Moen, Ebenezer Addei, Eduardo Salinas
 Description: Routes for the SQLAlchemy application
 '''
 from app import app, db, load_user
@@ -11,33 +12,34 @@ from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, login_user, logout_user, current_user
 import bcrypt
 
+
 @app.route('/')
 @app.route('/index')
 @app.route('/index.html')
-
 def index():
     return render_template('index.html')
+
 
 @app.route('/user/signup', methods=['GET', 'POST'])
 def user_signup():
     form = SignUpForm()
-    
+
     if form.validate_on_submit():
         passwd = form.passwd.data
         passwd_confirm = form.passwd_confirm.data
-        
+
         # Check if passwords match
         if passwd != passwd_confirm:
             return '<p>Passwords do not match</p>'
-        
+
         # Hash the password
         hashed = bcrypt.hashpw(passwd.encode('utf-8'), bcrypt.gensalt())
-        
+
         # Check if a user with the given ID already exists
         existing_user = User.query.filter_by(id=form.id.data).first()
         if existing_user:
             return '<p>User with this ID already exists!</p>'
-        
+
         # Create a new user
         new_user = User(
             id=form.id.data,
@@ -45,14 +47,15 @@ def user_signup():
             profile_picture=form.profile_picture.data,
             passwd=hashed
         )
-        
+
         # Add the new user to the database and commit
         db.session.add(new_user)
         db.session.commit()
-        
+
         return redirect(url_for('index'))
-    
+
     return render_template('user_signup.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -61,7 +64,6 @@ def login():
     if form.validate_on_submit():
         id = form.id.data
         passwd = form.passwd.data
-
 
         user = User.query.filter_by(id=id).first()
 
@@ -79,12 +81,13 @@ def login():
 # This will log out the user and remove their information from the session
 @app.route('/user/signout')
 def user_signout():
-    logout_user()  
+    logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/user/signout')
 def admin_signout():
-    logout_user()  
+    logout_user()
     return redirect(url_for('index'))
 
 
@@ -108,11 +111,13 @@ def save_post():
 def motaverse():
     all_posts = Post.query.order_by(Post.id.desc()).limit(100).all()
     all_users = User.query.all()
-    current_user_profile_pic_url = url_for('static', filename='pic/' + current_user.profile_picture)
+    current_user_profile_pic_url = url_for(
+        'static',
+        filename='pic/' + current_user.profile_picture)
     current_user_display_name = current_user.name
 
     return render_template(
-        'motaverse.html', 
+        'motaverse.html',
         all_users=all_users,
         current_user_profile_pic_url=current_user_profile_pic_url,
         current_user_display_name=current_user_display_name,
